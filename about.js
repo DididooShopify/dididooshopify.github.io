13072f498533f7aa598cca933cc5b8fe4a769451
Edward Tang <edward.tang@shopify.com>
2019-11-14 20:34:39 -0500

    Complete About and Projects pages

---
+++ about.js
@@ -0,0 +1,47 @@
+// window.onload = () => {
+//   let background = document.querySelector("body").style.backgroundImage;
+//   const newBackground = function(imageURL, background) {
+//     background = `url("${imageURL}#" + new Date().getTime())`;
+//   };
+
+//   const oldBackground = () => {
+//     background = `url("images/guangzhoudark.jpg")`;
+//   };
+
+//   let pianist = document.getElementById("pianist");
+//   pianist.addEventListener('click', function(){newBackground("images/piano.jpg", background)});
+//   // pianist.addEventListener('mouseout', oldBackground);
+// };
+
+window.onload = () => {
+  const roblox = document.getElementById("roblox");
+
+  let flexify = () => {
+    let glasses = document.getElementById('glasses');
+    let audio = new Howl({
+      src: ['https://manzdev.github.io/cursos-assets/js/thug-life.mp3'],
+    });  
+    if (!audio.playing()) {
+      glasses.style.animationPlayState = 'running';
+      audio.play();
+      let portrait = document.getElementById("portrait");
+      portrait.style.WebkitFilter = "grayscale(100%)";
+      portrait.style.filter = "grayscale(100%)";
+      roblox.innerHTML += " (Net Worth: 24K+ Robux)";
+      roblox.removeEventListener('mouseenter', enter);
+      roblox.removeEventListener('click', flexify)
+    }
+  }
+
+  let enter = function() {
+    roblox.style.color = 'rgb(252,231,134)';
+  }
+
+  let leave = function() {
+    roblox.style.color = 'rgb(41, 218, 82)';
+  }
+
+  roblox.addEventListener('mouseenter', enter);
+  roblox.addEventListener('mouseleave', leave);
+  roblox.addEventListener('click', flexify);
+}
