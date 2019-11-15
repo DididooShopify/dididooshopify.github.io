940f06374a8ff7f6c7ad3db003540ed1a5d6acf2
Edward Tang <edward.tang@shopify.com>
2019-11-14 22:12:20 -0500

    Adjust naming of variables

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 window.onload = () => {
     
-  const display_box = document.querySelector("h1");
+  const text = document.querySelector("h1");
 
   let string = "Hi, I'm Edward Tang.";
   let chars = string.split("");
 
   let i = 0
   let animation = setInterval(function() {
-      display_box.innerHTML += chars[i];
+      text.innerHTML += chars[i];
       i++;
       if (i == string.length) { 
         clearInterval(animation);
